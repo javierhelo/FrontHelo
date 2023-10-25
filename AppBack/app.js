04513549f460fa3061b32964f7c364526fae93e5
Javier Helo <javierhelo@gmail.com>
2023-10-25 01:52:21 -0600

    login y registro

---
+++ AppBack/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+require('dotenv').config();  //cargar variables en .env (a traves deprocess)
+
+const app = express();
+
+//Config
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server is listening on port " + PORT);
+})
