10d50bc53131890deb918185bf7909bf41a0937e
Javier Helo <javierhelo@gmail.com>
2023-11-03 04:19:30 -0600

    port change

--- AppBack/app.js
+++ AppBack/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-const PORT = process.env.PORT || 3000;
+const PORT = 4200;
 app.listen(PORT, () => {
     console.log("Server is listening on port " + PORT);
 })
